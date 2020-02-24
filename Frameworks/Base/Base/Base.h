1da30279e8d5ae8594b024cf0fe58cf9defc5656
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-02-24 21:22:52 +0300

    Изменено название framework-а

---
+++ Frameworks/Base/Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by Михаил Борисов on 24.02.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
