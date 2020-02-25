caa24cca9b9a3709e3280f84d8752b27b6baf7d6
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-02-26 00:12:37 +0300

    Добавлены Constraint и действие для свича

--- Frameworks/Base/Base/Classes/SignView.swift
+++ Frameworks/Base/Base/Classes/SignView.swift
@@ -13,7 +13,7 @@ open class SignView: UIViewController {
     lazy var slider: UISwitch = {
         let slider = UISwitch()
         slider.setOn(false, animated: true)
-        //slider.addTarget(self, action: #selector(slideAction(_:)), for: .valueChanged)
+        slider.addTarget(self, action: #selector(slideAction(_:)), for: .valueChanged)
         return slider
     }()
 
@@ -33,11 +33,60 @@ open class SignView: UIViewController {
 
     override open func loadView() {
         super.loadView()
-
+        prepareStackView()
+        prepareConstraints()
     }
 
     override open func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
     }
+}
+
+// MARK: Prepare UI
+private extension SignView {
+
+    func prepareStackView() {
+        view.addSubview(vStackView)
+        view.addSubview(onboardingTitleLabel)
+        view.addSubview(signButton)
+        hStackView.addArrangedSubview(slider)
+        hStackView.addArrangedSubview(fourPinLabel)
+        vStackView.addArrangedSubview(emailTextField)
+        vStackView.addArrangedSubview(passwordTextField)
+        vStackView.addArrangedSubview(hStackView)
+    }
+
+    func prepareConstraints() {
+        NSLayoutConstraint.activate([
+            emailTextField.heightAnchor.constraint(equalToConstant: 46),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 46),
+            vStackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            vStackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            vStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 60),
+            vStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -60),
+
+            onboardingTitleLabel.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            onboardingTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                      constant: view.bounds.height * 0.1),
+            signButton.heightAnchor.constraint(equalToConstant: 46),
+            signButton.leadingAnchor.constraint(equalTo: vStackView.leadingAnchor),
+            signButton.trailingAnchor.constraint(equalTo: vStackView.trailingAnchor),
+            signButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70)
+        ])
+    }
+
+}
+
+@objc private extension SignView {
+
+    func slideAction(_ sender: UISwitch) {
+        switch sender.isOn {
+        case true:
+            signButton.toggleState(state: true)
+        case false:
+            signButton.toggleState(state: false)
+        }
+    }
+
 }
