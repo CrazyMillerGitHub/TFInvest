a8de9234c9c8df003bda53f2ab3957cc45ac926c
Саидов Тимур <tim77777@inbox.ru>
2020-02-25 11:19:48 +0300

    Добавлены базовые сущности

--- Frameworks/Base/Assembly.swift
+++ Frameworks/Base/Assembly.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+public protocol ConfiguratorsContainerProtocol {
+    func configurator<T: BaseConfigurator>() -> T
+}
+open class ConfiguratorsContainer: ConfiguratorsContainerProtocol {
+    
+    public func configurator<T: BaseConfigurator>() -> T {
+        let configurator = T.init()
+        return configurator
+    }
+}
+
+public protocol ModulesContainerProtocol {
+    func viewController<T: BaseController>() -> T
+    func viewModel<U: BaseViewModel>(params: Decodable?) -> U
+}
+open class ModulesContainer: ModulesContainerProtocol {
+    
+    public func viewController<T>() -> T where T : BaseController {
+        return T.init()
+    }
+    
+    public func viewModel<U>(params: Decodable?) -> U where U : BaseViewModel {
+        return U.init(params: params)
+    }
+}
+
+public protocol ServicesContainerProtocol {
+    func service<T: BaseService>() -> T
+}
+open class ServicesContainer: ServicesContainerProtocol {
+    
+    public func service<T: BaseService>() -> T {
+        return T.init()
+    }
+}
--- Frameworks/Base/Base.swift
+++ Frameworks/Base/Base.swift
@@ -5,4 +5,53 @@
 //  Created by Саидов Тимур on 25.02.2020.
 //
 
-import Foundation
+import UIKit
+
+public protocol BaseControllerProtocol: class {
+    var viewModel: BaseViewModelProtocol? { get set }
+    var configuratorsContainer: ConfiguratorsContainerProtocol? { get set }
+    var modulesContainer: ModulesContainerProtocol? { get set }
+}
+public protocol BaseViewModelProtocol: class {
+    var params: Decodable? { get set }
+    var viewController: BaseControllerProtocol? { get set }
+    var servicesContainer: ServicesContainerProtocol? { get set }
+}
+
+public class BaseController: UIViewController, BaseControllerProtocol {
+    
+    public var viewModel: BaseViewModelProtocol?
+    public var configuratorsContainer: ConfiguratorsContainerProtocol?
+    public var modulesContainer: ModulesContainerProtocol?
+}
+public class BaseViewModel: BaseViewModelProtocol {
+    
+    public weak var viewController: BaseControllerProtocol?
+    public var servicesContainer: ServicesContainerProtocol?
+    public var params: Decodable?
+
+    required public init(params: Decodable?) {
+        self.params = params
+    }
+}
+
+public protocol ConfiguratorProtocol {
+    func configure<T: BaseControllerProtocol, U: BaseViewModelProtocol>(with viewController: T, _ viewModel: U)
+    func configure<T: BaseControllerProtocol, U: BaseViewModelProtocol, Z: ServicesContainerProtocol>(with viewController: T, _ viewModel: U, servicesContainer: Z)
+    func configure<T: BaseControllerProtocol, U: BaseViewModelProtocol, V: ConfiguratorsContainerProtocol, W: ModulesContainerProtocol, Z: ServicesContainerProtocol>(with viewController: T, _ viewModel: U,
+                                                                                                                                                                      configuratorsContainer: V, modulesContainer: W, servicesContainer: Z)
+}
+public class BaseConfigurator: ConfiguratorProtocol {
+    
+    required public init() {}
+    
+    public func configure<T, U>(with viewController: T, _ viewModel: U) where T : BaseControllerProtocol, U : BaseViewModelProtocol {}
+    public func configure<T, U, Z>(with viewController: T, _ viewModel: U, servicesContainer: Z) where T : BaseControllerProtocol, U : BaseViewModelProtocol, Z : ServicesContainerProtocol {}
+    public func configure<T, U, V, W, Z>(with viewController: T, _ viewModel: U,
+                                         configuratorsContainer: V, modulesContainer: W, servicesContainer: Z) where T : BaseControllerProtocol, U : BaseViewModelProtocol, V : ConfiguratorsContainerProtocol, W : ModulesContainerProtocol, Z : ServicesContainerProtocol {}
+}
+
+public class BaseService {
+    
+    required public init() {}
+}
