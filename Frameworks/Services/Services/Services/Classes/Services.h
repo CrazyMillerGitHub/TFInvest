2253b90dbc0b6351f081715a05641a878ecaa9f3
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-02-28 11:12:37 +0300

    Добавлен сервис Services

---
+++ Frameworks/Services/Services/Services/Classes/Services.h
@@ -0,0 +1,19 @@
+//
+//  Services.h
+//  Services
+//
+//  Created by Михаил Борисов on 28.02.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Services.
+FOUNDATION_EXPORT double ServicesVersionNumber;
+
+//! Project version string for Services.
+FOUNDATION_EXPORT const unsigned char ServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Services/PublicHeader.h>
+
+
