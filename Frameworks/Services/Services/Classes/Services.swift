7fa8bfbf8a6005f93aebaa2a2cec54f34b71d49e
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-02-28 11:34:23 +0300

    Создан Pod Service (#9)
    * Изменена директория файлов
    
    * Добавлен сервис Services
    
    * Удалена лишняя папка
    
    * добавлен pod в Podfile

---
+++ Frameworks/Services/Services/Classes/Services.swift
@@ -0,0 +1,14 @@
+//
+//  Services.swift
+//  Services
+//
+//  Created by Михаил Борисов on 28.02.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+import UIKit
+
+public class Services {
+
+
+}
