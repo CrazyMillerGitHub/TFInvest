2cd573efd0445fcd6d9689dfb8b34cfabcc91a10
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-03-01 14:53:47 +0300

    Добавлен Core

---
+++ Frameworks/Core/Core/Classes/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Михаил Борисов on 01.03.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
