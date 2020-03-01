ae4acce8bd000d82c1f707bc98a375909e1b516f
Mikhail Borisov <mishaborisov2013@gmail.com>
2020-03-01 15:11:11 +0300

    Добавлен framework Assembly (#15)

---
+++ Frameworks/Assembly/Assembly/Classes/Assembly.h
@@ -0,0 +1,19 @@
+//
+//  Assembly.h
+//  Assembly
+//
+//  Created by Михаил Борисов on 01.03.2020.
+//  Copyright © 2020 Mikhail Borisov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Assembly.
+FOUNDATION_EXPORT double AssemblyVersionNumber;
+
+//! Project version string for Assembly.
+FOUNDATION_EXPORT const unsigned char AssemblyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Assembly/PublicHeader.h>
+
+
